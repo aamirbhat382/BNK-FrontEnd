21197b70e44adca75140204d89ef7b7d95a7cdf9
Aamir Bhat <aamirbhat382@gmail.com>
2021-06-05 11:14:10 +0530

    Add uploade img

--- js/tiny.js
+++ js/tiny.js
@@ -104,3 +104,25 @@ tinymce.init({
     content_css: useDarkMode ? 'dark' : 'default',
     content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
 });
+window.addEventListener('load', ready)
+
+function ready() {
+    FilePond.registerPlugin(
+
+        // encodes the file as base64 data
+        FilePondPluginFileEncode,
+        FilePondPluginFileValidateSize
+
+    );
+    FilePond.setOptions({
+        allowFileSizeValidation: true,
+        maxFileSize: '10MB',
+        labelMaxFileSizeExceeded: 'File is too large'
+    })
+
+
+    // Select the file input and use create() to turn it into a pond
+    FilePond.create(
+        document.querySelector('.filepond', )
+    );
+}
